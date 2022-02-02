352060e54f7c8e7fe385534c13787b7054750179
ihsan <ihsan@kominfo.surabaya.go.id>
2022-02-02 11:32:00 +0700

    add gh

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  siteMetadata: {
-    title: ` `,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+	parhPrefix:"/landingpage",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
